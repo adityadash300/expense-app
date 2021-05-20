11b2208a9d8bd80486ffa293b23d86a06f75420c
adityadash300 <63318643+adityadash300@users.noreply.github.com>
2021-05-20 11:32:00 +0530

    Delete src/tests directory

--- src/tests/components/dashboard-page.test.js
+++
@@ -1,8 +0,0 @@
-import shallow from "enzyme/build/shallow"
-import React from "react"
-import ExpenseDashboardPage from "../../components/dashboard-page"
-
-test('should render ExpenseDashboardPage correctly', () => {
-    const wrapper = shallow(<ExpenseDashboardPage />)
-    expect(wrapper).toMatchSnapshot()
-})
--- src/tests/components/not-found-page.test.js
+++
@@ -1,8 +0,0 @@
-import shallow from "enzyme/build/shallow"
-import React from "react"
-import NotFoundPage from "../../components/not-found-page"
-
-test('should render NotFoundPage correctly', () => {
-    const wrapper = shallow(<NotFoundPage />)
-    expect(wrapper).toMatchSnapshot()
-})

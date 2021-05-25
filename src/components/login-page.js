58696930d3fa04baea79d8461b8545602aae426c
adityadash300 <63318643+adityadash300@users.noreply.github.com>
2021-05-25 20:44:06 +0530

    added google logo for log in

--- src/components/login-page.js
+++ src/components/login-page.js
@@ -8,7 +8,7 @@ export const LoginPage = ({ startLogin }) => (
         <div className="box-layout__box">
             <h1 className="box-layout__title">Expensify</h1>
             <p>It's time to get your expenses under control</p>
-            <button onClick={startLogin}  className="button">Login with Google</button>
+            <button onClick={startLogin}  className="button button--login">Login with <img className="box-layout__image" src="/images/google-logo.jpg"></img></button>
         </div>
     </div>
 )
